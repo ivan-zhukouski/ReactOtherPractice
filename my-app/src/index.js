621ee1f4cda10e77d6496a4fc34e69e33a6decfb
VanoVeryWonderful <stigmatix.by@gmail.com>
2019-05-23 00:55:09 +0300

    0:55)))

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Иван-Царевич';
-const element = <h1>Здравствуй, {name}!</h1>;
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+function tick(){
+  const element = (
+    <div>
+      <h1>Hello World</h1>
+      <h2>Now {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+};
+
+
+setInterval(tick, 1000);
